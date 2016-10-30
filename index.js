a2bcf9916d9a5cfeb1999a2d9a1c379e7aa3bc77
kfrn <knfrances@gmail.com>
2016-10-31 12:03:01 +1300

    creates a new student

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var knex = Knex(knexConfig)
 
 //Create
 function createStudent(student, callback) {
-  
+  return knex.raw(`INSERT INTO students (name) VALUES ("${student}")`).asCallback(callback)
 }
 
 //Read
@@ -14,19 +14,25 @@ function getAllStudents(callback) {
 
 //Read
 function getStudentById(id, callback) {
-  
+
 }
 
 //Update
 function updateStudent(student, callback) {
-  
+
 }
 
 //Delete
 function deleteStudentById(id, callback) {
-  
+
 }
 
+
+
+createStudent('Pouramua Kupenga', function( err, res) {
+  if (err) return console.log(err)
+  console.log(res);
+})
 
 //Let's try out the getAllStudents function.
 getAllStudents(function(err, res) {
