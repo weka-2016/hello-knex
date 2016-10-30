3722c2fb230f0c315ccb3ed055d657b5f878995e
bradymat <bradymat@gmail.com>
2016-10-31 11:37:19 +1300

    back to the shadow realm

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ var Knex = require('knex')
 var knexConfig = require('./knexfile.js').development;
 var knex = Knex(knexConfig)
 
+var studentEx = {
+  name: "Damn Dante",
+  cohort: "The Shadow Realm"
+}
+
 //Create
 function createStudent(student, callback) {
-  
+  //return knex.raw(`INSERT INTO students(name, cohort) VALUES("${student.name}", "${student.cohort}")`).asCallback(callback)
+  return knex('students').insert(student).asCallback(callback)
 }
 
 //Read
@@ -14,22 +20,46 @@ function getAllStudents(callback) {
 
 //Read
 function getStudentById(id, callback) {
-  
+  return knex('students').where({id: id}).asCallback(callback);
 }
 
 //Update
-function updateStudent(student, callback) {
-  
+function updateStudent(callback) {
+  return knex('students').where('name','=','Harrison').update({name: "Harry"}).asCallback(callback)
 }
 
 //Delete
-function deleteStudentById(id, callback) {
-  
+function deleteStudentById(callback) {
+  return knex('students').where('cohort', '=','The Shadow Realm').del().asCallback(callback)
 }
+
+deleteStudentById(function(err,res) {
+  if(err) console.log(err)
+  console.log(res);
+})
+
+updateStudent(function(err,res) {
+  if (err) console.log(err)
+  console.log(res);
+})
+
+getStudentById('4', function(err, res) {
+  if (err) return console.log(err)
+  console.log(res)
+})
+
+createStudent(studentEx, function(err, res) {
+
+  if (err) return console.log(err)
+  getAllStudents(function(err, res) {
+    if(err) return  console.log(err)
+    console.log(res)
+  })
+})
 
 
 //Let's try out the getAllStudents function.
 getAllStudents(function(err, res) {
   if(err) return  console.log(err)
-  console.log(res)
+  //console.log(res)
 })
