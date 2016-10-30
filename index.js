237a7bc005a12ab156d2d34e2557cf0f1a2536d0
harrison-symes <symeshjb@gmail.com>
2016-10-31 11:42:34 +1300

    add getStudentById

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var knex = Knex(knexConfig)
 
 //Create
 function createStudent(student, callback) {
-  
+  knex.raw(`Insert into students ('name', 'cohort') values ('${student.name}','${student.cohort}')`).asCallback(callback)
 }
 
 //Read
@@ -14,22 +14,33 @@ function getAllStudents(callback) {
 
 //Read
 function getStudentById(id, callback) {
-  
+  return knex.select().from('students').where('id', id).asCallback(callback)
+
 }
 
 //Update
 function updateStudent(student, callback) {
-  
+
 }
 
 //Delete
 function deleteStudentById(id, callback) {
-  
+
 }
 
 
 //Let's try out the getAllStudents function.
-getAllStudents(function(err, res) {
+// getAllStudents(function(err, res) {
+//   if(err) return  console.log(err)
+//   console.log(res)
+// })
+
+// createStudent({name:"Jasmine", cohort:"Piwakawaka-2016"}, function(err,res){
+//   if(err) return console.log(err)
+//   console.log(res)
+// })  WORKING!
+
+getStudentById(1, function(err, res) {
   if(err) return  console.log(err)
   console.log(res)
 })
