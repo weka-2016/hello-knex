48ed56fcf565796f93e085b347230010bf161efb
rodrigo-delgado <rod.delgado85@gmail.com>
2016-10-31 11:30:02 +1300

    get student by id working

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var knex = Knex(knexConfig)
 
 //Create
 function createStudent(student, callback) {
-  
+
 }
 
 //Read
@@ -14,17 +14,17 @@ function getAllStudents(callback) {
 
 //Read
 function getStudentById(id, callback) {
-  
+return knex.raw('SELECT name, cohort FROM students WHERE id = ' + id).asCallback(callback)
 }
 
 //Update
 function updateStudent(student, callback) {
-  
+
 }
 
 //Delete
 function deleteStudentById(id, callback) {
-  
+
 }
 
 
@@ -33,3 +33,10 @@ getAllStudents(function(err, res) {
   if(err) return  console.log(err)
   console.log(res)
 })
+
+function checkIfError(err, res){
+  if(err) return console.log(err)
+  console.log(res)
+}
+
+getStudentById(1, checkIfError)
