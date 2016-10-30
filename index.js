a563fbc38878019280d49f18d69c323a865bd4fc
kfrn <knfrances@gmail.com>
2016-10-31 11:18:42 +1300

    Add SQL query to createStudent; call FN

--- index.js
+++ index.js
@@ -4,32 +4,38 @@ var knex = Knex(knexConfig)
 
 //Create
 function createStudent(student, callback) {
-  
+  return knex.raw("INSERT INTO students (name, cohort) VALUES ('Katherine', 'Piwakawaka-2016')").asCallback(callback)
 }
 
 //Read
 function getAllStudents(callback) {
-  return knex.select().table('students').asCallback(callback)
+  return knex.raw('SELECT * FROM students').asCallback(callback)
 }
 
 //Read
 function getStudentById(id, callback) {
-  
+
 }
 
 //Update
 function updateStudent(student, callback) {
-  
+
 }
 
 //Delete
 function deleteStudentById(id, callback) {
-  
+
 }
 
 
 //Let's try out the getAllStudents function.
 getAllStudents(function(err, res) {
   if(err) return  console.log(err)
+  console.log(res)
+})
+
+// createStudent FN
+createStudent(function(err, res) {
+  if(err) return console.log(err)
   console.log(res)
 })
