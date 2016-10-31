415629480bc3ddb8eb37c26249659fead8f937fa
Mel Booth <mel.m.booth@gmail.com>
2016-10-31 20:39:52 +1300

    update createStudent function

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ var Knex = require('knex')
 var knexConfig = require('./knexfile.js').development;
 var knex = Knex(knexConfig)
 
-//Create
-function createStudent(student, callback) {
-  
+var mel = {
+  name: 'mel',
+  cohort: 'piwakawaka-2016'
 }
+
+function createStudent(student, callback) {
+  // return knex.raw(`INSERT INTO students(name, cohort) VALUES("${student.name}", "${student.cohort}")`).asCallback(callback)
+  return knex('students').insert({name: student.name, cohort: student.cohort}).asCallback(callback)
+}
+
+createStudent(mel, function(err, res){
+  if (err) return console.log(err)
+  console.log(res)
+})
 
 //Read
 function getAllStudents(callback) {
@@ -14,17 +24,17 @@ function getAllStudents(callback) {
 
 //Read
 function getStudentById(id, callback) {
-  
+
 }
 
 //Update
 function updateStudent(student, callback) {
-  
+
 }
 
 //Delete
 function deleteStudentById(id, callback) {
-  
+
 }
 
 
