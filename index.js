d3874ea9cbdc1d17227f49be3231249d59d1f8b5
Mel Booth <mel.m.booth@gmail.com>
2016-10-31 21:11:24 +1300

    update updateStudent and deleteStudent functions

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function createStudent(student, callback) {
 
 createStudent(mel, function(err, res){
   if (err) return console.log(err)
-  console.log(res)
+  console.log(res[student])
 })
 
 //Read
@@ -24,21 +24,37 @@ function getAllStudents(callback) {
 
 //Read
 function getStudentById(id, callback) {
-
+  return knex.select().from('students').where('id', id).asCallback(callback)
 }
 
-//Update
+getStudentById(1, function(err, res){
+  if (err) return console.log(err)
+  console.log(res)
+})
+
+// //Update
 function updateStudent(student, callback) {
-
+  return knex('students').where({name: student.name}).update({cohort: student.cohort}).asCallback(callback)
 }
 
-//Delete
+updateStudent({name: 'katie', cohort: 'blah-2016'}, function(err, res){
+  if (err) return console.log(err)
+  console.log(res)
+})
+// // updateStudent works but console logs number 1, why??
+
+// //Delete
 function deleteStudentById(id, callback) {
-
+  return knex('students').where('id', id).del().asCallback(callback)
 }
 
+deleteStudentById(2, function(err, res){
+  if (err) return console.log(err)
+  console.log(res)
+})
+//deleteStudent works but console logs a number? Why?
 
-//Let's try out the getAllStudents function.
+// Let's try out the getAllStudents function.
 getAllStudents(function(err, res) {
   if(err) return  console.log(err)
   console.log(res)
