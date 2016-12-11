1f25954962ca364b911b4765a58b2a9de95af642
anna-marie-vos <hughes.meghan@gmail.com>
2016-12-12 12:36:09 +1300

    initial exersize up to update

--- index.js
+++ index.js
@@ -3,28 +3,50 @@ var knexConfig = require('./knexfile.js').development;
 var knex = Knex(knexConfig)
 
 //Create
-function createStudent(student, callback) {
-  
-}
+//     // declare the student to be added
+// var student = {name:'bobby',cohort:'weka-2010'}
+//      // create the function to  insert the student
+// function createStudent(student, callback) {
+//   return knex('students').insert(student).asCallback(callback)
+// }
+//     // run the function
+// createStudent(student,function(err, res) {
+//   if(err) return  console.log(err)
+//   console.log(res)
+// })
 
 //Read
-function getAllStudents(callback) {
-  return knex.select().table('students').asCallback(callback)
+    //get all the students
+function getAllStudents(id,callback) {
+  return knex.select('*').table('students').asCallback(callback)
 }
 
+var id = 1;
 //Read
 function getStudentById(id, callback) {
-  
+// return knex.select().table('students').asCallback(callback)
+knex.raw('SELECT * FROM students WHERE id = ?;', [id]).asCallback(callback)
+}
+//call the student by ID function
+getStudentById(id,function(err,res){
+  if(err) return  console.log(err)
+  console.log(res)
+})
+
+//Update ...not working
+var newStudent = "hector,weka-2010"
+function updateStudent(newStudent, callback) {
+knex.raw('INSERT INTO students (name, cohort)VALUES ('+newStudent+');')
 }
 
-//Update
-function updateStudent(student, callback) {
-  
-}
+updateStudent(newStudent,function(err,res){
+  if(err) return  console.log(err)
+  console.log(res)
+})
 
 //Delete
 function deleteStudentById(id, callback) {
-  
+
 }
 
 
