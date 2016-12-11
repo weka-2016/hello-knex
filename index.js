c5d6549a648bd613f51dea9a0b427313edcbbd5c
mischa-saunders <mischa.saunders@gmail.com>
2016-12-12 12:22:05 +1300

    initial commit

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var knex = Knex(knexConfig)
 
 //Create
 function createStudent(student, callback) {
-  
+    return knex('students').insert(student).asCallback(callback)
+  // return knex.insert([{name: "Lucas"},  {cohort: "Weka 2016"}], 'id').into('students').asCallback(callback)
 }
 
 //Read
@@ -14,22 +15,30 @@ function getAllStudents(callback) {
 
 //Read
 function getStudentById(id, callback) {
-  
+  return knex.select(id).table('students').asCallback(callback)
+
 }
 
 //Update
 function updateStudent(student, callback) {
-  
+
 }
 
 //Delete
 function deleteStudentById(id, callback) {
-  
+
 }
 
-
 //Let's try out the getAllStudents function.
-getAllStudents(function(err, res) {
-  if(err) return  console.log(err)
+// createStudent({name: 'Lucas', cohort: '2016'}, function (err, res){
+//   if(err) return  console.log(err)
+//   // console.log(res)
+// })
+// getAllStudents(function(err, res) {
+//   if(err) return  console.log(err)
+//   console.log(res)
+// })
+getStudentById(2, function (err,res){
+  if(err) return console.log(err)
   console.log(res)
 })
