432f28717c10c26d247ca848a801edd91c72300f
Janie Tito <janietito@Admins-MacBook-Air.local>
2016-12-12 22:46:43 +1300

    can update a student name by ID number

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ var knex = Knex(knexConfig)
 
 //Create
 function createStudent(student, callback) {
-  
+  return knex
+  .insert({
+    name: 'Amy',
+    cohort: 'piwakawaka-2016'
+  })
+  .into('students')
+  .asCallback(callback)
 }
 
 //Read
@@ -12,24 +18,50 @@ function getAllStudents(callback) {
   return knex.select().table('students').asCallback(callback)
 }
 
-//Read
-function getStudentById(id, callback) {
-  
-}
+// //Read
+// function getStudentById(id, callback) {
+//   return knex ('students')
+//   .select('id', [1])
+//   .asCallback(callback)
+// }
 
 //Update
 function updateStudent(student, callback) {
-  
+  return knex ('students')
+  .where('id', '=', '6')
+  .update({
+    name: "Silly Sausage",
+    cohort: "Chicken"
+  })
+  .asCallback(callback)
 }
 
 //Delete
 function deleteStudentById(id, callback) {
-  
+
 }
 
+//*****CALL ALL FUNCTIONS**********//
 
 //Let's try out the getAllStudents function.
 getAllStudents(function(err, res) {
   if(err) return  console.log(err)
   console.log(res)
 })
+
+//call the createStudent function
+createStudent(function(err, res){
+  if(err) return console.log(err)
+  console.log(res)
+})
+
+//call the updateStudent function
+updateStudent(function(err,res) {
+  if(err) return console.log(err)
+  console.log(res)
+})
+
+// getStudentById(id, function(err, res){
+//   if(err) return console.log(err)
+//   console.log(id)
+// })
